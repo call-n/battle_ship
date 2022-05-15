1d5c85d445fd78883a2f3b3488700a323bc25565
ebyrd <ebyrdeu.khnykin@gmail.com>
2022-05-15 19:47:57 +0200

    styling for game lobby

---
+++ client/src/components/gameLobby/CustomTextField.js
@@ -0,0 +1,17 @@
+import {styled, TextField,} from "@mui/material";
+
+const CustomTextField = styled(TextField)`
+
+  label {
+    color: white;
+  }
+
+  div {
+    input {
+      color: white;
+
+    }
+  }
+`;
+
+export default  CustomTextField;
