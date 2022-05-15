b7bf3a182072873f17e1a130258d75b9b7e1b7d8
ebyrd <ebyrdeu.khnykin@gmail.com>
2022-05-15 19:47:32 +0200

    styling for background

---
+++ client/src/components/bokeh/Bokeh.jsx
@@ -0,0 +1,28 @@
+import './bokeh.css'
+const Bokeh = () => {
+	return (
+		<div className="background">
+			<span></span>
+			<span></span>
+			<span></span>
+			<span></span>
+			<span></span>
+			<span></span>
+			<span></span>
+			<span></span>
+			<span></span>
+			<span></span>
+			<span></span>
+			<span></span>
+			<span></span>
+			<span></span>
+			<span></span>
+			<span></span>
+			<span></span>
+			<span></span>
+			<span></span>
+		</div>
+	);
+};
+
+export default Bokeh;
